380b3717f3cc2eb5735d307efe99d0505b874033
olgaklim29 <79910695+olgaklim29@users.noreply.github.com>
2021-11-04 14:14:12 +0100

    hooked

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,32 +1,67 @@
+import { useState } from "react";
 import FeedbackOptions from "../FeedbackOptions/FeedbackOptions";
 import Statistics from "../Statistics/Statistics";
 import Notification from "../Notification/Notification";
 import styles from "./Title.module.css";
 
-export default function Title({
-  onLeaveFeedback,
-  good,
-  neutral,
-  bad,
-  total,
-  positivePercentage,
-  textObj,
-  options,
-}) {
+const text = {
+  grandTitleTxt: "Please leave feedback",
+  titleTxt: "Statistics",
+  goodContent: "Good:",
+  neutralContent: "Neutral:",
+  badContent: "Bad:",
+  totalContent: "Total:",
+  positivePercentageContent: "Positive feedback:",
+};
+
+const options = ["good", "neutral", "bad"];
+
+export default function Title() {
+  const [counterGood, setCounterGood] = useState(0);
+  const [counterNeutral, setCounterNeutral] = useState(0);
+  const [counterBad, setCounterBad] = useState(0);
+
+  const onLeaveFeedback = (e) => {
+    const { name } = e.target;
+    switch (name) {
+      case "good":
+        setCounterGood((prevCounterGood) => prevCounterGood + 1);
+        break;
+      case "neutral":
+        setCounterNeutral((prevCounterNeutral) => prevCounterNeutral + 1);
+        break;
+      case "bad":
+        setCounterBad((prevCounterBad) => prevCounterBad + 1);
+        break;
+      default:
+        console.warn(`Butoon - ${name} is under constraction`);
+    }
+  };
+  const countTotalFeedback = () => {
+    return counterGood + counterNeutral + counterBad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return counterGood
+      ? Math.ceil((counterGood / countTotalFeedback()) * 100) + "%"
+      : 0 + "%";
+  };
+
   const { section, title, title2 } = styles;
+  const { grandTitleTxt, titleTxt } = text;
   return (
     <section className={section}>
-      <h1 className={title}>{textObj.grandTitleTxt}</h1>
+      <h1 className={title}>{grandTitleTxt}</h1>
       <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
-      <h2 className={title2}>{textObj.titleTxt}</h2>
-      {good || neutral || bad ? (
+      <h2 className={title2}>{titleTxt}</h2>
+      {counterGood || counterNeutral || counterBad ? (
         <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage}
-          textObj={textObj}
+          good={counterGood}
+          neutral={counterNeutral}
+          bad={counterBad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+          textObj={text}
         />
       ) : (
         <Notification message="No feedback given" />
